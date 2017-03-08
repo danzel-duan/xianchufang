8181bc9cbb91bb2d2cea8c4caf2531dbce66ef56
zhenglong duan <zhenglongduan@zhenglongs-iMac.local>
2017-03-08 09:04:36 +0800

    消除警告

---
+++ DLoadingView/DLoadingView/DLoadingView/DLoadAnimationView.h
@@ -0,0 +1,13 @@
+//
+//  DLoadAnimationView.h
+//  DLoadingView
+//
+//  Created by zhenglong duan on 07/03/2017.
+//  Copyright © 2017 zhenglong duan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DLoadAnimationView : UIView
+
+@end
---
+++ DLoadingView/DLoadingView/DLoadingView/DLoadAnimationView.m
@@ -0,0 +1,21 @@
+//
+//  DLoadAnimationView.m
+//  DLoadingView
+//
+//  Created by zhenglong duan on 07/03/2017.
+//  Copyright © 2017 zhenglong duan. All rights reserved.
+//
+
+#import "DLoadAnimationView.h"
+
+@implementation DLoadAnimationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
